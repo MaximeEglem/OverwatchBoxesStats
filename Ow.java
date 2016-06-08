5f3aa52192ae6f950d69c09dfb6288ccf552eef1
Maxxy <Maxxi@users.noreply.github.com>
2016-06-08 14:10:59 +0200

    Add items classes
    Allow me to manage items per heroe

--- Ow.java
+++ Ow.java
@@ -22,3 +22,116 @@ class HeroeItems {
 }
 
 
+
+class CommunItems extends Items {
+
+    CommunItems(int numberItems) {
+        super(Rarities.COMMUN.toString(), 75, numberItems, 58.66);
+    }
+}
+
+class RareItems extends Items {
+
+    RareItems(int numberItems) {
+        super(Rarities.RARE.toString(), 75, numberItems, 31.69);
+    }
+}
+
+class EpicItems extends Items {
+
+    EpicItems(int numberItems) {
+        super(Rarities.EPIC.toString(), 250, numberItems, 7.22);
+    }
+}
+
+class LegendaryItems extends Items {
+
+    LegendaryItems(int numberItems) {
+        super(Rarities.LEGENDARY.toString(), 1000, numberItems, 2.43);
+    }
+}
+
+class Items {
+
+    private String type;
+    private int    price;
+    private int    number;
+    private double pourcentage;
+
+    Items(String type, int price, int number, double pourcentage) {
+        this.type = type;
+        this.price = price;
+        this.number = number;
+        this.pourcentage = pourcentage;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type
+     *            the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price
+     *            the price to set
+     */
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number
+     *            the number to set
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    /**
+     * @return the pourcentage
+     */
+    public double getPourcentage() {
+        return pourcentage;
+    }
+
+    /**
+     * @param pourcentage
+     *            the pourcentage to set
+     */
+    public void setPourcentage(double pourcentage) {
+        this.pourcentage = pourcentage;
+    }
+
+}
+
+class Heroes {
+
+   
+    Heroes() {
+    }
+
+
+}
