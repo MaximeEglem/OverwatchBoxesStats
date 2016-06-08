3a39219dc96b371f379adcc68c145d740a1a6fda
Maxxy <Maxxi@users.noreply.github.com>
2016-06-08 14:24:26 +0200

    Create items logic
    Manage all items in 4 types of rarities

--- Ow.java
+++ Ow.java
@@ -1,24 +1,31 @@
-package com.overwatch.stats;
+package fr.swisslife.front.menu.enumeration;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class Ow {
 
-    
+    static private Heroes heroes;
+    static int[]          communStack;
+    static int            communItemsNotOwn;
+    static int[]          rareStack;
+    static int            rareItemsNotOwn;
+    static int[]          epicStack;
+    static int            epicItemsNotOwn;
+    static int[]          legendaryStack;
+    static int            legendaryItemsNotOwn;
+    static int            gold        = 0;
+    static int            boxesNeeded = 0;
+    static int            ItemsNotOwn;
+    static int            indexRandom;
+
     public static void main(String[] args) {
-        
+
+      
     }
 
-    private static void spendGoldForItem(){}
-      
-}
-
-enum Rarities {
-    
-}
-
-class HeroeItems {
+    private static void spendGoldForItem() {
+       }
 }
 
 enum Rarities {
@@ -41,6 +48,54 @@ enum Rarities {
     public String toString() {
         return rarity;
     }
+}
+
+class HeroeItems {
+
+    protected CommunItems    commun;
+    protected RareItems      rare;
+    protected EpicItems      epic;
+    protected LegendaryItems legendary;
+
+    HeroeItems() {
+    }
+
+    HeroeItems(int communNb, int rareNb, int epicNb, int legendaryNb) {
+        commun = new CommunItems(communNb);
+        rare = new RareItems(rareNb);
+        epic = new EpicItems(epicNb);
+        legendary = new LegendaryItems(epicNb);
+
+    }
+
+    /**
+     * @return the commun
+     */
+    public CommunItems getCommun() {
+        return commun;
+    }
+
+    /**
+     * @return the rare
+     */
+    public RareItems getRare() {
+        return rare;
+    }
+
+    /**
+     * @return the epic
+     */
+    public EpicItems getEpic() {
+        return epic;
+    }
+
+    /**
+     * @return the legendary
+     */
+    public LegendaryItems getLegendary() {
+        return legendary;
+    }
+
 }
 
 class Bastion extends HeroeItems {
@@ -359,9 +414,139 @@ class Items {
 
 class Heroes {
 
-   
+    final private List<HeroeItems> heroes       = new ArrayList<HeroeItems>();
+    private CommunItems            allCommun    = new CommunItems(0);
+    private RareItems              allRare      = new RareItems(0);
+    private EpicItems              allEpic      = new EpicItems(0);
+    private LegendaryItems         allLegendary = new LegendaryItems(0);
+    private int                    total        = 0;
+
     Heroes() {
+        heroes.add(new Bastion());
+        heroes.add(new DVa());
+        heroes.add(new Genji());
+        heroes.add(new Hanzo());
+        heroes.add(new Junkrat());
+        heroes.add(new Lucio());
+        heroes.add(new McCree());
+        heroes.add(new Mei());
+        heroes.add(new Mercy());
+        heroes.add(new Pharah());
+        heroes.add(new Reaper());
+        heroes.add(new Reinhardt());
+        heroes.add(new Roadhog());
+        heroes.add(new Soldier76());
+        heroes.add(new Symmetra());
+        heroes.add(new Torbjorn());
+        heroes.add(new Tracer());
+        heroes.add(new Widowmaker());
+        heroes.add(new Winston());
+        heroes.add(new Zarya());
+        heroes.add(new Zenyatta());
+
+        getAllItems();
     }
 
+    private void getAllItems() {
+        for (HeroeItems heroe : heroes) {
+            allCommun.setNumber(allCommun.getNumber()
+                    + totalItems(heroe.getCommun().getNumber()));
+            allRare.setNumber(allRare.getNumber()
+                    + totalItems(heroe.getRare().getNumber()));
+            allEpic.setNumber(allEpic.getNumber()
+                    + totalItems(heroe.getEpic().getNumber()));
+            allLegendary.setNumber(allLegendary.getNumber()
+                    + totalItems(heroe.getLegendary().getNumber()));
+        }
+        System.out.println(toString());
+    }
+
+    /**
+     * @return the allCommun
+     */
+    public CommunItems getAllCommun() {
+        return allCommun;
+    }
+
+    /**
+     * @param allCommun
+     *            the allCommun to set
+     */
+    public void setAllCommun(CommunItems allCommun) {
+        this.allCommun = allCommun;
+    }
+
+    /**
+     * @return the allRare
+     */
+    public RareItems getAllRare() {
+        return allRare;
+    }
+
+    /**
+     * @param allRare
+     *            the allRare to set
+     */
+    public void setAllRare(RareItems allRare) {
+        this.allRare = allRare;
+    }
+
+    /**
+     * @return the allEpic
+     */
+    public EpicItems getAllEpic() {
+        return allEpic;
+    }
+
+    /**
+     * @param allEpic
+     *            the allEpic to set
+     */
+    public void setAllEpic(EpicItems allEpic) {
+        this.allEpic = allEpic;
+    }
+
+    /**
+     * @return the allLegendary
+     */
+    public LegendaryItems getAllLegendary() {
+        return allLegendary;
+    }
+
+    /**
+     * @param allLegendary
+     *            the allLegendary to set
+     */
+    public void setAllLegendary(LegendaryItems allLegendary) {
+        this.allLegendary = allLegendary;
+    }
+
+    /**
+     * @return the total
+     */
+    public int getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total
+     *            the total to set
+     */
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    private int totalItems(int number) {
+        this.total += number;
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return heroes.size() + " Heroes [allCommun=" + allCommun.getNumber()
+                + ", allRare=" + allRare.getNumber() + ", allEpic="
+                + allEpic.getNumber() + ", allLegendary="
+                + allLegendary.getNumber() + ", Total=" + total + "]";
+    }
 
 }
